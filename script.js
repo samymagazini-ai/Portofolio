6154f7fd6cd3dc46c990539c42d341dd17810c43
unknown <samymagazini@gmail.com>
2025-11-08 08:53:31 +0000

    Mes fichiers

---
+++ script.js
@@ -0,0 +1,7 @@
+// Menu responsive
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
